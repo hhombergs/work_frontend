48bac4a8d00f69fafb453139cf80c4eb7334e301
Heinz Hombergs <hh@etacs.de>
2017-08-30 15:13:37 +0200

    Remove the id display.

--- src/flats/index.js
+++ src/flats/index.js
@@ -7,7 +7,6 @@ import FlatDeleteButton from './FlatDeleteButton';
 export const FlatList = (props) => (
     <List {...props} title="Alle Wohnungen" sort={{ field: 'enter_date', order: 'DESC' }} perPage={25}>
         <Datagrid>
-            <TextField source="id" label="ID" />
             <DateField source="enter_date" options={{day: '2-digit', month:'2-digit', year: 'numeric'}} label="Einzugsdatum" />
             <TextField source="street" label="Straße" />
             <TextField source="zip" label="Postleitzahl" />
@@ -45,7 +44,6 @@ const FlatTitle = ({ record }) => record ? <FullTitleField record={record} size=
 export const FlatEdit = (props) => (
     <Edit actions={<FlatEditActions />} title={<FlatTitle />} {...props}>
         <SimpleForm>
-            <DisabledInput label="Id" source="id" />
             <DateInput source="enter_date" label="Einzugsdatum" />
             <TextInput source="street" label="Strasse" />
             <TextInput source="zip" label="PLZ" />
