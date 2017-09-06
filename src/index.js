ba368bf8a211b91fbe8fd3feff929a9d8f66c0cc
Heinz Hombergs <hh@etacs.de>
2017-09-06 15:52:08 +0200

    Start linting and lint the src/index.js

--- src/index.js
+++ src/index.js
@@ -4,28 +4,19 @@ import App from './App';
 import FlatApp from './FlatApp';
 import './index.css';
 
-// This one is already present in admin-on-rest Layout, but it seems it does nothing if called after the initial ReactDOM.render()
-// @link https://github.com/callemall/material-ui/issues/4670#issuecomment-235031917
-import injectTapEventPlugin from 'react-tap-event-plugin';
-try {
-    injectTapEventPlugin();
-} catch (e) {
-    // do nothing
-}
-
-var isMain = true;
+let isMain = true;
 if (window.location.href.indexOf('/flat/') !== -1) {
     isMain = false;
 }
 
-if (isMain == true) {
+if (isMain === true) {
     ReactDOM.render(
-      <App />,
-      document.getElementById('root')
+        <App />,
+        document.getElementById('root'),
     );
 } else {
     ReactDOM.render(
-      <FlatApp />,
-      document.getElementById('root')
+        <FlatApp />,
+        document.getElementById('root'),
     );
 }
