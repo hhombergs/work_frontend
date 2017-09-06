d0a898acea4da37bd3b73ed1067b6f26fdb01758
Heinz Hombergs <hh@etacs.de>
2017-09-06 15:59:01 +0200

    Lint the App.js file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import 'babel-polyfill';
-import React, { Component } from 'react';
-import { Admin, Resource} from 'admin-on-rest';
+import React from 'react';
+import { Admin, Resource } from 'admin-on-rest';
 import myApiRestClient from './restClient';
 import './App.css';
 import germanMessages from './i18n';
@@ -8,17 +8,13 @@ import { FlatList, FlatEdit, FlatCreate, FlatShow } from './flats';
 import FlatDelete from './flats/FlatDelete';
 
 const messages = {
-    'de': germanMessages,
+    de: germanMessages,
 };
 
-class App extends Component {
-    render() {
-        return(
-            <Admin restClient={myApiRestClient} title="Wohnungen" locale="de" messages={messages}>
-                <Resource name="flats" options={{ label: 'Wohnungen' }} list={FlatList} edit={FlatEdit} create={FlatCreate} show={FlatShow} remove={FlatDelete} />
-            </Admin>
-        );
-    };
-};
+const App = () => (
+    <Admin restClient={myApiRestClient} title="Wohnungen" locale="de" messages={messages}>
+        <Resource name="flats" options={{ label: 'Wohnungen' }} list={FlatList} edit={FlatEdit} create={FlatCreate} show={FlatShow} remove={FlatDelete} />
+    </Admin>
+);
 
 export default App;
