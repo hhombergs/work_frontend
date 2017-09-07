5f1c9a253eb24014fa80688313d9a272dabd8913
Heinz Hombergs <hh@etacs.de>
2017-09-07 08:22:43 +0200

    Lint the src/FlatApp.js and src/flats/FullTitleField.js

--- src/FlatApp.js
+++ src/FlatApp.js
@@ -1,28 +1,21 @@
 import 'babel-polyfill';
-import React, { Component } from 'react';
-import { Admin, Resource} from 'admin-on-rest';
-import { Route } from 'react-router-dom';
-
+import React from 'react';
+import { Admin, Resource } from 'admin-on-rest';
 import myApiRestClient from './restClient';
 import './App.css';
 import germanMessages from './i18n';
-import { FlatEdit, FlatShow, FlatList} from './flat';
+import { FlatEdit, FlatShow, FlatList } from './flat';
 import FlatDelete from './flat/FlatDelete';
 import authClient from './authClient';
 
 const messages = {
-    'de': germanMessages,
+    de: germanMessages,
 };
 
-class FlatApp extends Component {
-
-    render() {
-        return(
-            <Admin authClient={authClient} restClient={myApiRestClient} title="Wohnung" locale="de" messages={messages}>
-                <Resource name="flat" options={{ label: 'Wohnung' }} edit={FlatEdit} remove={FlatDelete} show={FlatShow} list={FlatList} />
-            </Admin>
-        );
-    };
-};
+const FlatApp = () => (
+    <Admin authClient={authClient} restClient={myApiRestClient} title="Wohnung" locale="de" messages={messages}>
+        <Resource name="flat" options={{ label: 'Wohnung' }} edit={FlatEdit} remove={FlatDelete} show={FlatShow} list={FlatList} />
+    </Admin>
+);
 
 export default FlatApp;
