595f7d9b2eb2cebea8c7a9674bde270ef0a54c0e
Heinz Hombergs <hh@etacs.de>
2017-09-07 10:47:36 +0200

    Lint src/flat/FullTitleField.js

--- src/flat/FullTitleField.js
+++ src/flat/FullTitleField.js
@@ -2,14 +2,22 @@
 * @Author: hhombergs
 * @Date:   2017-08-29
 * @Last Modified by:   hhombergs
-* @Last Modified time: 2017-08-29
+* @Last Modified time: 2017-09-07
 */
 
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const FullTitleField = ({ record = {}, size = 85 }) => <span>
-    <span style={{ display: 'inline-block', width: size/3 }}>&nbsp;</span>
-    {record.street}, {record.zip}, {record.city}, {record.country}
-</span>;
+const FullTitleField = ({ record = {}, size = 85 }) => (
+    <span>
+        <span style={{ display: 'inline-block', width: size / 3 }}>&nbsp;</span>
+        {record.street}, {record.zip}, {record.city}, {record.country}
+    </span>
+);
+
+FullTitleField.propTypes = {
+    record: PropTypes.object,
+    size: PropTypes.number,
+};
 
 export default FullTitleField;
