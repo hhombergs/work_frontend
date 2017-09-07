2447950b2340f6652bf4f8685817491bd5822cab
Heinz Hombergs <hh@etacs.de>
2017-09-07 08:30:08 +0200

    Lint src/flats/index.js

--- src/flats/index.js
+++ src/flats/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { CardActions } from 'material-ui/Card';
-import {Create, Datagrid, DateField, DateInput, DeleteButton, DisabledInput, Edit, EditButton, EmailField, List, ListButton, RefreshButton, Show,  ShowButton, SimpleForm, SimpleShowLayout, TextField, TextInput, } from 'admin-on-rest';
+import { Create, Datagrid, DateField, DateInput, DeleteButton, Edit, EditButton, EmailField, List, Show, ShowButton, SimpleForm, SimpleShowLayout, TextField, TextInput } from 'admin-on-rest';
+import PropTypes from 'prop-types';
 import FullTitleField from './FullTitleField';
 
-export const FlatList = (props) => (
+export const FlatList = props => (
     <List {...props} title="Alle Wohnungen" sort={{ field: 'enter_date', order: 'DESC' }} perPage={25}>
         <Datagrid>
-            <DateField source="enter_date" options={{day: '2-digit', month:'2-digit', year: 'numeric'}} label="Einzugsdatum" />
+            <DateField source="enter_date" options={{ day: '2-digit', month: '2-digit', year: 'numeric' }} label="Einzugsdatum" />
             <TextField source="street" label="Straße" />
             <TextField source="zip" label="Postleitzahl" />
             <TextField source="city" label="Ort" />
@@ -17,18 +17,16 @@ export const FlatList = (props) => (
             <DeleteButton />
         </Datagrid>
     </List>
-)
+);
 
-const dateString = v => {
-  if (isNaN(v)) return;
-
-  let parsedDate = new Date(v);
-  let adjustedDate = new Date(parsedDate.getTime() - parsedDate.getTimezoneOffset() * 60000);
-
-  return adjustedDate;
+const dateString = (v) => {
+    if (isNaN(v)) return;
+    const parsedDate = new Date(v);
+    const adjustedDate = new Date(parsedDate.getTime() - (parsedDate.getTimezoneOffset() * 60000));
+    return adjustedDate;
 };
 
-export const FlatCreate = (props) => (
+export const FlatCreate = props => (
     <Create title="Wohnung anlegen" {...props}>
         <SimpleForm redirect="list">
             <DateInput source="enter_date" label="Einzugsdatum" validation={{ required: true }} parse={dateString} />
@@ -41,9 +39,13 @@ export const FlatCreate = (props) => (
     </Create>
 );
 
-const FlatTitle = ({ record }) => record ? <FullTitleField record={record} size={32} /> : null;
+const FlatTitle = ({ record }) => (record ? <FullTitleField record={record} size={32} /> : null);
 
-export const FlatEdit = (props) => (
+FlatTitle.propTypes = {
+    record: PropTypes.object,
+};
+
+export const FlatEdit = props => (
     <Edit title={<FlatTitle />} {...props}>
         <SimpleForm>
             <DateInput source="enter_date" label="Einzugsdatum" parse={dateString} />
@@ -56,7 +58,7 @@ export const FlatEdit = (props) => (
     </Edit>
 );
 
-export const FlatShow = (props) => (
+export const FlatShow = props => (
     <Show title={<FlatTitle />} {...props}>
         <SimpleShowLayout>
             <DateField source="enter_date" label="Einzugsdatum" />
@@ -64,7 +66,7 @@ export const FlatShow = (props) => (
             <TextField source="zip" label="PLZ" />
             <TextField source="city" label="Ort" />
             <TextField source="country" label="Land" />
-            <TextField source="contact_email" type="email" label="Kontakt Email"/>
+            <TextField source="contact_email" type="email" label="Kontakt Email" />
         </SimpleShowLayout>
     </Show>
 );
