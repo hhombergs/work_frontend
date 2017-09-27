d87b896c5ad41d578e077931d777b58ffc45d0e5
Heinz Hombergs <hh@etacs.de>
2017-09-27 12:25:06 +0200

    Add the login to the mainpage

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import { Admin, Resource } from 'admin-on-rest';
 import myApiRestClient from './restClient';
 import './styles/App.scss';
 import germanMessages from './i18n';
+import authClient from './authClientMain';
 import { FlatList, FlatEdit, FlatCreate, FlatShow } from './flats';
 import FlatDelete from './flats/FlatDelete';
 
 const messages = {
     de: germanMessages,
 };
+messages.de.aor.auth.username = 'Bentuzername';
 
 const App = () => (
-    <Admin restClient={myApiRestClient} title="Wohnungen" locale="de" messages={messages}>
+    <Admin restClient={myApiRestClient} title="Wohnungen" locale="de" messages={messages} authClient={authClient}>
         <Resource name="flats" options={{ label: 'Wohnungen' }} list={FlatList} edit={FlatEdit} create={FlatCreate} show={FlatShow} remove={FlatDelete} />
     </Admin>
 );
